32c1480435c0231936b71f350758a6d6a3dfc164
liuyu <373966902@qq.com>
2017-08-21 17:02:41 +0800

    初始化框架

---
+++ resources/views/admin/default/index.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layouts.master')
+@section('title','仪表盘')
+@section('content-wrapper')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                仪表盘
+                <small>控制面板</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="{{ url('admin') }}"><i class="fa fa-dashboard"></i>首页</a></li>
+                <li class="active">仪表盘</li>
+            </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            <!-- Your Page Content Here -->
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
